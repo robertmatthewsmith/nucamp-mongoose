aadd3883509ffa5406bacea926c00a4175974a98
Robert Smith <robertmatthewsmith1976@gmail.com>
2021-01-06 09:59:59 -0600

    Mongoose Part 3

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Campsite = require('./models/campsite');
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -19,7 +20,23 @@ connect.then(() => {
     })
     .then(campsite => {
         console.log(campsite);
-        return Campsite.find();
+
+        return Campsite.findByIdAndUpdate(campsite._id,{
+            $set: { description: 'Updated Test Document' }
+        }, {
+            new: true
+        });
+    })
+    .then(campsite => {
+        console.log(campsite);
+
+        campsite.comments.push({
+            rating: 5,
+            text: 'What a magnificent view!',
+            author: 'Tinus Lorvaldes'
+        });
+
+        return campsite.save();
     })
     .then(campsites => {
         console.log(campsites);
